e7c25b419c615b1789f435d8f1c2b6aa8dd0c527
MadhuraShanbhag <41289059+MadhuraShanbhag@users.noreply.github.com>
2018-10-07 19:18:11 +0530

    Update urls.py

--- IET_Hackathon/IET_Hackathon/urls.py
+++ IET_Hackathon/IET_Hackathon/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from Login import views as login_views
 
 urlpatterns = [
-    url(r'^signup/$', login_views.signup_view, name='signup'),
     url(r'^login/$', login_views.login_view, name='login'),
     url(r'^admin/', admin.site.urls),
     url(r'^blogs/', include('Blog.urls')),
